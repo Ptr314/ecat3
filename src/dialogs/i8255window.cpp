af11c07ad082ba384c717845a0f1b40c4962d540
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2023-09-21 00:31:59 +0300

    8255 dialog fixes

--- src/dialogs/i8255window.cpp
+++ src/dialogs/i8255window.cpp
@@ -40,18 +40,18 @@ void I8255Window::update()
     unsigned int vc = d->get_value(2);
     unsigned int vd = d->get_value(3);
     ui->binA->setText(QString("%1").arg(va, 8, 2, QChar('0')));
-    ui->hexA->setText(QString("%1").arg(va, 2, 16, QChar('0')));
+    ui->hexA->setText(QString("%1").arg(va, 2, 16, QChar('0')).toUpper());
     ui->binB->setText(QString("%1").arg(vb, 8, 2, QChar('0')));
-    ui->hexB->setText(QString("%1").arg(vb, 2, 16, QChar('0')));
+    ui->hexB->setText(QString("%1").arg(vb, 2, 16, QChar('0')).toUpper());
     ui->binCH->setText(QString("%1").arg(vc >> 4, 4, 2, QChar('0')));
     ui->binCL->setText(QString("%1").arg(vc & 0x0F, 4, 2, QChar('0')));
-    ui->hexC->setText(QString("%1").arg(vc, 2, 16, QChar('0')));
+    ui->hexC->setText(QString("%1").arg(vc, 2, 16, QChar('0')).toUpper());
     ui->binD->setText(QString("%1").arg(vd, 8, 2, QChar('0')));
-    ui->hexD->setText(QString("%1").arg(vd, 2, 16, QChar('0')));
+    ui->hexD->setText(QString("%1").arg(vd, 2, 16, QChar('0')).toUpper());
 
-    ui->modeA->setText(((vd &  0x10) != 0)?"Input":"Output");
-    ui->modeB->setText(((vd &  0x02) != 0)?"Input":"Output");
-    ui->modeCH->setText(((vd & 0x08) != 0)?"Input":"Output");
-    ui->modeCL->setText(((vd & 0x01) != 0)?"Input":"Output");
+    ui->modeA->setText(((vd &  0x10) != 0)?I8255Window::tr("Input"):I8255Window::tr("Output"));
+    ui->modeB->setText(((vd &  0x02) != 0)?I8255Window::tr("Input"):I8255Window::tr("Output"));
+    ui->modeCH->setText(((vd & 0x08) != 0)?I8255Window::tr("Input"):I8255Window::tr("Output"));
+    ui->modeCL->setText(((vd & 0x01) != 0)?I8255Window::tr("Input"):I8255Window::tr("Output"));
 
 }
