b7a71fef5fe568dbd812bc52426f1b7cc58c6295
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2023-10-18 19:13:54 +0300

    page mapper device

--- src/emulator/devices/common/page_mapper.h
+++ src/emulator/devices/common/page_mapper.h
@@ -1,7 +1,10 @@
 #ifndef PAGE_MAPPER_H
 #define PAGE_MAPPER_H
 
+#include <QException>
+
 #include "emulator/core.h"
+#include "emulator/utils.h"
 
 class PageMapper:public AddressableDevice
 {
@@ -28,19 +31,52 @@ public:
     virtual void load_config(SystemData *sd) override
     {
         ComputerDevice::load_config(sd);
-        //TODO: page mapper
+
+        QString parameter_name, range;
+        unsigned int page_id;
+
+        for (unsigned int i = 0; i < cd->parameters_count; i++)
+        {
+            parameter_name = cd->parameters[i].name;
+            if (parameter_name == "@page")
+            {
+                range = cd->parameters[i].left_range;
+                if (range.isEmpty()) {
+                    QMessageBox::critical(0, MemoryMapper::tr("Error"), MemoryMapper::tr("Incorrect range for '%1'").arg(parameter_name));
+                    return;
+                }
+                page_id = parse_numeric_value(range.mid(1, range.length()-2));
+                if (page_id >= PagesCount) PagesCount = page_id + 1;
+                pages[page_id] =dynamic_cast<Memory*>(im->dm->get_device_by_name(cd->parameters[i].value));
+            }
+        }
+
+        PageMask = create_mask(round(log2(PagesCount+1)), 0);
+
+        try {
+            Frame = parse_numeric_value(cd->get_parameter("frame").value);
+        } catch (QException &e) {
+            Frame = pages[0]->get_size();
+        }
+
+        SegmentMask = create_mask(round(log2(pages[0]->get_size() / Frame)), 0);
     }
 
     virtual unsigned int get_value(unsigned int address) override
     {
-        return 0;
+        if (Frame == pages[0]->get_size())
+            return pages[i_page->value & PageMask]->get_value(address);
+        else
+            return pages[i_page->value & PageMask]->get_value((i_segment->value & SegmentMask)*Frame +  address);
     }
+
     virtual void set_value(unsigned int address, unsigned int value) override
     {
-
+        if (Frame == pages[0]->get_size())
+            pages[i_page->value & PageMask]->set_value(address, value);
+        else
+            pages[i_page->value & PageMask]->set_value((i_segment->value * SegmentMask)*Frame + address, value);
     }
-
-
 };
 
 
