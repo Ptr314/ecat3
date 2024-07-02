5638eafee5ece3e3c93f1e51319f789d1e829f0c
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2024-07-02 17:35:22 +0300

    bump version

--- src/globals.h
+++ src/globals.h
@@ -3,9 +3,9 @@
 
 #define PROJECT_NAME "eCat3"
 #define VERSION_MAJOR "3"
-#define VERSION_MINOR "0"
+#define VERSION_MINOR "1"
 #define VERSION_PATCH "0"
-#define PROJECT_VERSION "3.0.0"
+#define PROJECT_VERSION "3.1.0"
 
 /* #undef EXTERNAL_Z80 */
 /* #undef CPU_STOPPED */
