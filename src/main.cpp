307576d741937a1937f92d8c2f75dff63a70331c
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-07-01 19:00:44 +0300

    linux fix

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 
 int main(int argc, char *argv[])
 {
+    qputenv("QT_QPA_PLATFORM", "xcb"); // Enforce using X11
+
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
