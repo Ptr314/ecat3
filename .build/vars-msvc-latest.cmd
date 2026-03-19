2a6cda6de53f01045ca221bdb9df6ebba36d04f8
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2026-03-19 16:08:41 +0300

    build impr

---
+++ .build/vars-msvc-latest.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+SET _ROOT_MSVC=C:\DEV\MSVC\msvc
+SET _ROOT_QT=C:\DEV\Qt\6.10.2\msvc2022_64
+SET _ROOT_STATIC_BIN=%_ROOT_QT%\bin
+SET _ROOT_MSVC_CMAKE=%_ROOT_QT%\bin\qt-cmake
+
+SET _ROOT_CMAKE=C:\DEV\Qt\Tools\CMake_64\bin
+SET _ROOT_NINJA=C:\DEV\Qt\Tools\Ninja
+
+SET PATH=%_ROOT_CMAKE%;%_ROOT_NINJA%;%_ROOT_QT%\bin;%_ROOT_SRC%;%PATH%
+
+REM Initialize Visual Studio build environment
+call "%_ROOT_MSVC%\setup_x64.bat"
