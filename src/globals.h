740a4b4a27f1a3df471ce476f58d9d1f7c9eea6e
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2026-04-03 19:42:11 +0300

    version bump

--- src/globals.h
+++ src/globals.h
@@ -7,9 +7,9 @@
 
 #define PROJECT_NAME "eCat3"
 #define VERSION_MAJOR "3"
-#define VERSION_MINOR "5"
-#define VERSION_PATCH "3"
-#define PROJECT_VERSION "3.5.3"
+#define VERSION_MINOR "6"
+#define VERSION_PATCH "0"
+#define PROJECT_VERSION "3.6.0"
 
 /* #undef EXTERNAL_Z80 */
 /* #undef CPU_STOPPED */
